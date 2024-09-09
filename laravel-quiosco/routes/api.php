7121b25b4965ce5da01d3dabbd7f2dcee3e59851
GamalielRV <gamaro968@gmail.com>
2024-09-09 11:23:19 -0600

    trabajar en el area de prodcutos

--- laravel-quiosco/routes/api.php
+++ laravel-quiosco/routes/api.php
@@ -1,6 +1,7 @@
 <?php
 
 use App\Http\Controllers\CategoriaController;
+use App\Http\Controllers\ProductoController;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
 
@@ -8,4 +9,6 @@ Route::get('/user', function (Request $request) {
     return $request->user();
 })->middleware('auth:sanctum');
 
-Route :: apiResource( '/categorias' , CategoriaController::class) ; // ruta para traer categorias
+Route :: apiResource( '/categorias' , CategoriaController::class) ;// ruta para traer categorias
+
+Route :: apiResource( '/productos' , ProductoController::class) ;// ruta para traer productos
