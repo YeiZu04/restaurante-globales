0101cc14323e6beb1862b3edae203000d025061d
GamalielRV <gamaro968@gmail.com>
2024-09-13 15:50:01 -0600

    first endpoint (insert) pedidos, ready for tests from react

---
+++ laravel-quiosco/app/Http/Controllers/PedidoController.php
@@ -0,0 +1,81 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use App\Models\Pedido;
+use App\Models\PedidoProducto;
+use Carbon\Carbon;
+use Illuminate\Support\Facades\Auth;
+use Illuminate\Http\Request;
+
+class PedidoController extends Controller
+{
+    /**
+     * Display a listing of the resource.
+     */
+    public function index()
+    {
+        //
+    }
+
+    /**
+     * Store a newly created resource in storage.
+     */
+    public function store(Request $request)
+    {
+        //almacenar una orden
+        $pedido = new Pedido;
+        $pedido->user_id = Auth::user()->id;
+        $pedido->total =$request->total;
+        $pedido->save();
+
+        //obtener el id del pedido
+        $id = $pedido->id;
+
+        //obtener los productos
+        $productos = $request->productos;
+
+        //formatear el arreglo
+        $pedido_producto =[];
+        foreach($productos as $producto){
+            $pedido_producto []=[
+                'pedido_id' => $id,
+                'producto_id' => $producto['id'],
+                'cantidad' => $producto['cantidad'],
+                'created_at' => Carbon::now(),
+                'updated_at' => Carbon::now()
+            ];
+        }
+
+        //almacenar en la base de datos
+        PedidoProducto :: insert($pedido_producto);
+
+        return[
+            'message' => 'pedido realizado estara listo en unos minutos'
+        ];
+    }
+
+    /**
+     * Display the specified resource.
+     */
+    public function show(Pedido $pedido)
+    {
+        //
+    }
+
+    /**
+     * Update the specified resource in storage.
+     */
+    public function update(Request $request, Pedido $pedido)
+    {
+        //
+    }
+
+    /**
+     * Remove the specified resource from storage.
+     */
+    public function destroy(Pedido $pedido)
+    {
+        //
+    }
+}
--- laravel-quiosco/routes/api.php
+++ laravel-quiosco/routes/api.php
@@ -5,12 +5,15 @@ use Illuminate\Support\Facades\Route;
 use App\Http\Controllers\AuthController;
 use App\Http\Controllers\ProductoController;
 use App\Http\Controllers\CategoriaController;
+use App\Http\Controllers\PedidoController;
 
 Route::middleware('auth:sanctum')->group(function(){
    Route::get ('/user', function (Request $request) {
         return $request->user();
     });
     Route::post('/logout', [AuthController::class, 'logout']);
+    Route::apiResource('/pedidos',PedidoController::class);
+    
 });
 
 Route :: apiResource( '/categorias' , CategoriaController::class) ;// ruta para traer categorias
